9031397a4578e35c0045bffbede789b23b5b9066
q-horton <106427075+q-horton@users.noreply.github.com>
2024-02-05 01:02:52 +1000

    Updated to include a 'roll' sequence before the prize is revealed.

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import tkinter as tk
 import RPi.GPIO as GPIO
 import signal
 import sys
+import time
 
 
 BUTTON_PIN = 14
@@ -13,7 +14,7 @@ def main():
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING,
-			callback=button_callback, bouncetime=250)
+			callback=button_callback, bouncetime=2500)
 	signal.signal(signal.SIGINT, exit_handler)
 	
 	global goodies
@@ -36,12 +37,22 @@ def exit_handler(sig, frame):
 	sys.exit(0)
 
 
+def randomiser_flash():
+	global goodies, app
+	app.update_display("Roll!", "white")
+	time.sleep(0.5)
+	for i in range(1, 6):
+		app.update_display("." * i, "white")
+		time.sleep(0.2)
+	reward, colour = goodies.draw_goodie()
+	app.update_display(reward, colour)
+
+
 def button_callback(channel):
-	global button_protect, goodies, app
+	global button_protect
 	if not button_protect:
 		button_protect = True
-		reward, colour = goodies.draw_goodie()
-		app.update_display(reward, colour)
+		randomiser_flash()
 		button_protect = False
 
 
